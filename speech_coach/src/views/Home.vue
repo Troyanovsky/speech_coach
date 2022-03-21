4aad3c1afa0bef86d60f58617c627273c4c9aa6b
troyanovsky <524619251@qq.com>
2022-03-21 23:17:31 +0800

    Changed section order in Home page

--- speech_coach/src/views/Home.vue
+++ speech_coach/src/views/Home.vue
@@ -1,18 +1,15 @@
+<script setup></script>
+
 <template>
   <div class="hero min-h-screen bg-base-200">
-    <div class="hero-content flex-col text-center lg:flex-row items-center">
-      <div class="rounded-lg shadow-lg w-4/5">
+    <div class="hero-content flex-col items-center lg:flex-row">
+      <h1 class="text-3xl font-bold text-center">
+        Speak with <span class="text-primary">Confidence</span>
+      </h1>
+      <div class="rounded-lg shadow-lg w-4/5 m-6">
         <img src="public/home_hero_1.png" class="rounded-lg w-full" />
       </div>
-
-      <div>
-        <h1 class="text-3xl font-bold text-center m-6">
-          Speak with <span class="text-primary">Confidence</span>
-        </h1>
-        <div class="items-center justify-center">
-          <button class="btn btn-primary">Get Started</button>
-        </div>
-      </div>
+      <button class="btn btn-primary">Get Started</button>
     </div>
   </div>
 </template>
