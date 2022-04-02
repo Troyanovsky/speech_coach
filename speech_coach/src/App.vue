20615ced83234e321b34f1eb00abf16456596c5a
troyanovsky <524619251@qq.com>
2022-04-02 17:31:32 +0800

    Added login modal UI

--- speech_coach/src/App.vue
+++ speech_coach/src/App.vue
@@ -1,12 +1,13 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <template>
   <!--
-<router-link to="/">Home</router-link>
+<router-link to="/home">Home</router-link>
+<router-link to="/main">Main</router-link>
+<router-link to="/login">Login</router-link>
 <router-link to="/about">About</router-link>
 -->
   <router-view></router-view>
--- speech_coach/src/views/Home.vue
+++ speech_coach/src/views/Home.vue
@@ -1,15 +1,52 @@
 <script setup></script>
 
 <template>
+  <!--Modal for log in-->
+  <input type="checkbox" id="login-modal" class="modal-toggle" />
+  <label for="login-modal" class="modal cursor-pointer modal-middle">
+    <label class="modal-box relative" for="">
+      <label
+        for="login-modal"
+        class="btn btn-sm btn-circle absolute right-2 top-2"
+        >✕</label
+      >
+      <h3 class="text-xl font-bold">Hello There! Please log in.</h3>
+      <div class="form-control w-full max-w-xs">
+        <label class="label">
+          <span class="label-text">Email</span>
+        </label>
+        <input
+          type="text"
+          placeholder="Your Email"
+          class="input input-bordered w-full max-w-xs"
+        />
+        <label class="label">
+          <span class="label-text">Password</span>
+        </label>
+        <input
+          type="password"
+          placeholder="Your Password"
+          class="input input-bordered w-full max-w-xs"
+        />
+      </div>
+      <div class="modal-action">
+        <label for="login-modal" class="btn btn-primary"> Log In </label>
+        <label for="login-modal" class="btn btn-primary"> Cancel </label>
+      </div>
+    </label>
+  </label>
+
+  <!--Landing page hero-->
   <div class="hero min-h-screen bg-base-200">
     <div class="hero-content flex-col items-center">
-      <h1 class="text-3xl font-bold text-center">
-        Speak with <span class="text-primary">Confidence</span>
-      </h1>
+      <h1 class="text-4xl font-bold text-center text-primary">Speech Coach</h1>
+      <h2 class="text-2xl text-center">Speak with Confidence</h2>
       <div class="rounded-lg shadow-lg w-4/5 m-6">
-        <img src="public/home_hero_1.png" class="rounded-lg w-full" />
+        <img src="/home_hero_1.png" class="rounded-lg w-full" />
       </div>
-      <button class="btn btn-primary">Get Started</button>
+      <label for="login-modal" class="btn btn-primary modal-button">
+        Get Started
+      </label>
     </div>
   </div>
 </template>
