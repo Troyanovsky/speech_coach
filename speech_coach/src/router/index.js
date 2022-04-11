62f13914218197fc1e1407b36b37df7f4f92c9cc
troyanovsky <524619251@qq.com>
2022-04-11 18:40:56 +0800

    Set up results page

--- speech_coach/src/router/index.js
+++ speech_coach/src/router/index.js
@@ -2,12 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
 import About from '../views/About.vue';
 import Record from '../views/Record.vue';
+import Results from '../views/Results.vue';
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
   { path: '/home', name: 'Home', component: Home },
   { path: '/record', name: 'Record', component: Record },
   { path: '/about', name: 'About', component: About },
+  { path: '/results', name: 'Results', component: Results },
 ];
 
 const router = createRouter({
